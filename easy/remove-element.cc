2b0db034b3a18f882756c243446d3a32f1f51a04
李立强 <kos1990l@gmail.com>
2014-11-13 20:18:36 +0800

    Create remove-element.cc

---
+++ easy/remove-element.cc
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeElement(int A[], int n, int elem) {
+        for (int i = 0; i < n; i++) {
+            if (A[i] == elem) {
+                A[i] = A[--n];
+                --i;
+            }
+        }
+        return n;
+    }
+};
